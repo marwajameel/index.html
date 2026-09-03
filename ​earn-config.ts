83967268023902373d22a7423657f1c8aa33804b
Jamil Ahmed Kalyal <223995654+marwajameel@users.noreply.github.com>
2026-09-03 05:17:36 +0500

    Create ​earn-config.ts

---
+++ ​earn-config.ts
@@ -0,0 +1,18 @@
+export const QUICKNODE_EARN_CONFIG = {
+  // یونیورسل پروکسی ایڈریس (تمام چینز پر ایک ہی ایڈریس کام کرے گا)
+  proxyAddress: "0x48b415841165304f7EFaa7D5dD5FC65cc7B4bd8e",
+  
+  // تمام سپورٹڈ چین آئی ڈیز
+  supportedChains: {
+    base: 8453,
+    ethereum: 1,
+    arbitrum: 42161,
+    optimism: 10,
+    polygon: 137,
+    unichain: 130,
+    monad: 143,
+  },
+  
+  // ڈیفالٹ نیٹ ورک Base Mainnet رکھا گیا ہے
+  defaultChainId: 8453,
+};
